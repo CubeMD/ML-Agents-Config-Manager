884afca0d164fa882f7efe2146ccf17184cdf32a
CubeMD <markelovdp@gmail.com>
2022-07-26 01:07:20 -0400

    Removed build, added generator and runner scripts, new bat files for running. Experiments on dino

---
+++ generate_configs.bat
@@ -0,0 +1,4 @@
+call activate ML-Agents-Config-Manager
+python config_generator.py --source-config-path=Configs/Dino_Continuous_Base.yaml
+call conda deactivate
+pause
---
+++ tensorboard.bat
@@ -0,0 +1,4 @@
+call activate ML-Agents-Config-Manager
+tensorboard --logdir=results
+call conda deactivate
+pause
