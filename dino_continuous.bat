9bdd40bd9d74f250ba93b56e4be781b8cfa73af1
CubeMD <markelovdp@gmail.com>
2022-07-26 01:40:43 -0400

    Clean up

--- dino_continuous.bat
+++ dino_continuous.bat
@@ -1,4 +1,4 @@
 call activate ML-Agents-Config-Manager
-python config_runner.py --folder_path=Configs/dino-continuous-new --num_parallel=4 --max_workers=12
+python config_runner.py --folder_path=Configs/dino-continuous --num_parallel=4 --max_workers=12
 call conda deactivate
 pause
--- generate_configs.bat
+++
@@ -1,4 +0,0 @@
-call activate ML-Agents-Config-Manager
-python config_generator.py --source-config-path=Configs/Dino_Continuous_Base.yaml
-call conda deactivate
-pause
